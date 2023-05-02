de4b5d82dfcaed5bddfce8dafbab070691ca65d3
Robert Bain <31903812+bainro@users.noreply.github.com>
2023-05-02 10:45:41 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,85 @@
-# clearpath_backup
-solution to clearpath's 2.0.1 vs 2.0.2 mismatch when restoring ROS melodic on the jackal robot
+# Robot Backup
+This package will automatically backup a remote robot's core files and directories to your local machine into a single archive. This package will also restore the backed up files onto a robot. This package can also handle backing up files on a robot running an older ROS distro (i.e. ROS Kinetic), and restoring the backed files on a robot running a newer ROS distro (i.e. ROS Melodic).
+
+## Usage - Backing things up
+Ensure you have ```sshpass``` and ```rsync``` installed:
+
+```sudo apt-get install sshpass```
+```sudo apt-get install rsync```
+
+Then run the backup script. It may take several minutes depending on the amount of data being transferred:
+
+```bash backup.sh backup_name [user@]hostname [password]```
+
+e.g.
+
+```bash backup.sh my_robot 10.0.1.42```
+
+The script will log into the robot using SSH using and create a backup called ```backup_name.tar.gz``` (or ```my_robot.tar.gz```, in the example above).  If your robot does not use the default Clearpath username "administrator" and default password "clearpath" you may specify the correct username & password as additional arguments.  For example, a Jackal using a Jetson TX2 would use:
+
+```bash backup.sh my_robot nvidia@10.0.1.42 nvidia```
+
+The backup script will copy the following data, which can be restored later (see below):
+
+- Home Folder: ```~/```
+- ```udev``` Rules: ```/etc/udev/rules.d```
+- Network Setup:
+  - ```/etc/network/interfaces```
+  - ```/etc/netplan```
+  - ```/etc/netplan```
+  - ```/etc/hostname```
+  - ```/etc/hosts```
+- IP Tables: ```/etc/iptables```
+- Bringup Files:
+  - ```/etc/ros/setup.bash```
+  - ```/etc/ros/$ROSDISTRO/ros.d```
+  - ```/usr/sbin/*start```
+  - ```/usr/sbin/*stop```
+- ```rosdep``` sources: ```/etc/ros/rosdep```
+- ```rc.local``` File: ```/etc/rc.local```
+- ```pip``` packages
+- ```systemd``` configuration: ```/etc/systemd/system```
+- ```apt``` sources: ```/etc/apt/sources.list.d```
+- ```apt``` packages
+- User Permission Groups
+
+## Usage - Restoring from a backup
+Before restoring from the backup you may find it helpful to completely reset your robot to its factory OS.  You can download OS installation images from Clearpath Robotics here: http://packages.clearpathrobotics.com/stable/images/latest/.  Simply download the image appropriate to your robot, write it to a USB drive using unetbootin (or a similar tool), and follow the instructions in your robot's user manual for reinstalling the OS.  You can find the user guides & tutorials here: https://support.clearpathrobotics.com/hc/en-us
+
+Once the OS has been reinstalled (or if you do not wish to reinstall the OS and just want to restore the backed-up files), simply copy the ```restore.sh``` script & the backup file you previously created onto the robot.  Then run
+
+```bash restore.sh backup_name```
+
+e.g.
+
+```bash restore.sh my_robot```
+
+Do not run the restore script as root, as doing so may result in errors.
+
+## Usage - Upgrading ROS Distro
+*WARNING* This feature is experimental and may not work 100% correctly yet.
+
+You can also use the ```upgrade.sh``` script to restore your backup & upgrade from the backup's ROS distro to the robot's current's ROS distro. For example, you can backup the contents on a robot running ROS Melodic, upgrade the robot to ROS Noetic, then use the ```upgrade.sh``` script to restore and upgrade all the backed up files and directories to ROS Noetic.
+
+To do this:
+1. Back up your robot as normal. 
+2. Download the desired installation image for your robot from http://packages.clearpathrobotics.com/stable/images/latest/ 
+3. Install the image as normal.
+4. Once the upgraded OS is installed, copy the backup ```tar.gz``` file & ```upgrade.sh``` to your robot and run the following command: ```bash upgrade.sh backup_name```
+
+This will do the same procedure as the ```restore.sh script```, described above, but also attempt to change any references of the old ROS distro to the new ROS distro. For example, if upgrading from ROS Melodic to ROS Noetic, certain known files & folders will also be migrated:
+
+- ```$HOME/.bashrc will``` have all instances of "melodic" replaced with "noetic"
+- ```/etc/ros/setup.bash``` will have all instances of "melodic" replaced with "noetic"
+- ```/etc/ros/melodic/*``` will be copied to ```/etc/ros/noetic/*```
+
+Note that user-generated files in the home folder, as well as any files the user modified themselves elsewhere on the system may not be correctly migrated; in this case it is incumbant upon the user to migrate their own files manually as necessary.
+
+We recommend using the default "No" option when asked to reinstall APT packages.  By default the upgrade script will make a file in the user's home folder called ```restore-apt.sh```, which will contain the names of all packages to be installed.  We advise double-checking this list and ensuring that all packages are correct before running ```restore-apt.sh```.
+
+## Troubleshooting
+The backup.sh script can hang if you have not previously SSH'd into your robot, or if its SSH keys have changed.  If this occurs, run the following command on your local computer:
+
+```mv ~/.ssh/known_hosts ~/.ssh/known_hosts.bak.$(date +"%Y%m%d%H%M")```
+
+Then ssh into your robot again, and add its signature to your known hosts.  Then run backup.sh again.
---
+++ upgrade.sh
@@ -0,0 +1,437 @@
+#!/bin/bash
+# @author       Chris Iverach-Brereton <civerachb@clearpathrobotics.com>
+# @author       David Niewinski <dniewinski@clearpathrobotics.com>
+# @description  Restores a backup of a robot's core files and directories, and upgrades relevant #               files and directories from the backup ROS distro to the robot's current ROS distro.
+
+############################## FUNCTION DEFINITIONS ############################
+
+function cleanup {
+  echo "Cleaning Up $(pwd)/$1"
+  rm -rf $1
+  echo "Done Cleaning"
+}
+
+function promptDefaultNo {
+  # $1: the prompt
+  # return: 0 for no, 1 for yes
+  read -r -p "$1 [y/N] " response
+  if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]];
+  then
+    echo 1
+  else
+    echo 0
+  fi
+}
+
+function promptDefaultYes {
+  # $1: the prompt
+  # return: 0 for no, 1 for yes
+  read -r -p "$1 [Y/n] " response
+  if [[ "$response" =~ ^([nN][oO]|[nN])$ ]];
+  then
+    echo 0
+  else
+    echo 1
+  fi
+}
+
+function changeRosDistroPackage {
+  # change all occurences of the ROS distro codename in an arbitrary string to the new codename
+  # usage: changeRosDistroPackage $from $to $package
+  FROM="ros-$1"
+  TO="ros-$2"
+  PKG=$3
+  echo "${PKG//$FROM/$TO}"
+}
+
+function changeRosDistroFile {
+  # change all occurences of the ROS distro codename in a file to the new codename
+  # this creates a backup of the original file too
+  # usage: changeRosDistroPackage $from $to $file
+  FROM="$1"
+  TO="$2"
+  FILE=$3
+
+  echo "Attempting to migrate ROS distro in file $FILE from $FROM to $TO"
+  if [ -f $FILE ];
+  then
+    cp $FILE $FILE.bak.$(date +"%Y%m%d%H%M%S")
+    REGEX="s/$FROM/$TO/"
+    CMD="sed -i '$REGEX' $FILE"
+    bash -c "$CMD"
+  else
+    echo "WARNING: $FILE does not exist. Skipping migration"
+  fi
+}
+
+function tryInstallApt {
+  # usage: tryInstallApt $package
+  PACKAGE=$1
+
+  sudo apt-get install --reinstall --yes $PACKAGE
+  RESULT=$?
+  if [ $RESULT -ne 0 ];
+  then
+    echo -e "[WARN] ${RED}Failed to install $PACKAGE ${NC}"
+
+    if [ ! -f $HOME/could_not_install_apt.sh ];
+    then
+      echo "#!/bin/bash" > $HOME/could_not_install_apt.sh
+      echo "# The following packages could not be reinstalled" >> $HOME/could_not_install_apt.sh
+      echo "sudo apt-get install --reinstall --yes \\" >> $HOME/could_not_install_apt.sh
+    fi
+
+    echo "    $PACKAGE \\" >> $HOME/could_not_install_apt.sh
+  fi
+}
+
+function tryInstallPip {
+  # usage: tryInstallPip $package $version
+
+  PKG=$1
+  VERSION=$2
+
+  pip install -Iv $PKG==$VERSION
+  RESULT=$?
+  if [ $RESULT -ne 0 ];
+  then
+    echo -e "[WARN] ${RED}Failed to install $PACKAGE v$VERSION ${NC}"
+
+    if [ ! -f $HOME/could_not_install_pip.sh ];
+    then
+      echo "#!/bin/bash" > $HOME/could_not_install_pip.sh
+      echo "# The following packages could not be reinstalled" >> $HOME/could_not_install_pip.sh
+    fi
+
+    echo "pip install $PKG==$VERSION" >> $HOME/could_not_install_pip.sh
+  fi
+
+}
+
+############################## ARGUMENT PARSING ############################
+
+# the username used during the original backup
+# by default on robots we ship this should always be "administrator"
+USERNAME=administrator
+
+# the version of _this_ script
+VERSION=2.0.2
+
+RED='\e[31m'
+GREEN='\e[32m'
+NC='\e[39m' # No Color
+
+if [ $# -ge 1 ]
+then
+  if [ $# == 2 ];
+  then
+    echo "Overriding default username"
+    USERNAME=$2
+    echo "Username: $USERNAME"
+  fi
+
+  CUSTOMER=$1
+
+  ######################## UNPACK ARCHIVE ##########################
+  echo "Unpacking backup"
+  tar -xf $CUSTOMER.tar.gz $CUSTOMER
+  cd $CUSTOMER
+
+  ############################ METADATA CHECK ###############################
+
+  echo "Checking backup version"
+  if [ ! -f "BKUP_VERSION" ];
+  then
+    echo "ERROR: no backup script version specified; cannot restore this backup!"
+    exit 1
+  else
+    BKUP_VERSION=$(cat BKUP_VERSION)
+    if [ "$VERSION" != "$BKUP_VERSION"  ];
+    then
+      cd ..
+      cleanup "$1"
+      echo "ERROR: backup was made with a different version; please download v$BKUP_VERSION of this script"
+      exit 1
+    else
+      echo "Backup can be restored with this script"
+    fi
+  fi
+
+  echo "Checking backup ROS distro"
+  if [ ! -f "ROS_DISTRO" ];
+  then
+    echo "ERROR: no ROS distro specified in backup. Aborting."
+    cd ..
+    cleanup "$1"
+    exit 1
+  fi
+  ROSDISTRO=$(cat ROS_DISTRO)
+  echo "ROS distro in backup is $ROSDISTRO"
+
+  OLD_ROSDISTRO="$ROSDISTRO"
+
+  echo "Checking current ROS distro"
+  ROSDISTRO=$(ls -rt /opt/ros | tail -1)
+  echo "ROS distro is $ROSDISTRO"
+    
+  echo "+++++ Upgrading from ROS $OLD_ROSDISTRO to $ROSDISTRO +++++"
+
+  if [ "$USERNAME" != "$(whoami)" ];
+  then
+    echo "WARNING: current user ($(whoami)) does not match expected account ($USERNAME)"
+    CONTINUE=$(promptDefaultNo "Continue?")
+    if [ $CONTINUE == 1 ]
+    then
+        echo "Ignoring username mismatch"
+    else
+        cd ..
+        cleanup "$1"
+        echo "User aborted"
+        exit 0
+    fi
+  fi
+
+  ############################ ROS & BRINGUP #############################
+  if [ -f setup.bash ];
+  then
+    echo "Restoring etc/ros/setup.bash"
+    sudo cp setup.bash /etc/ros/setup.bash
+
+    sudo bash -c "$(declare -f changeRosDistroFile); changeRosDistroFile $OLD_ROSDISTRO $ROSDISTRO /etc/ros/setup.bash"
+  else
+    echo "Skipping setup.bash; no backup"
+  fi
+
+  if [ -d ros.d ];
+  then
+    echo "Restoring Bringup"
+    sudo mkdir -p /etc/ros/$ROSDISTRO/ros.d
+    sudo cp -r ros.d/. /etc/ros/$ROSDISTRO/ros.d
+  else
+    echo "Skipping bringup; no backup"
+  fi
+
+  if [ -d usr/sbin ];
+  then
+    echo "Restoring sbin"
+    sudo cp -r usr/sbin/. /usr/sbin
+  else
+    echo "Skipping sbin; no backup"
+  fi
+
+  if [ -d rosdep ];
+  then
+    echo "Restoring RosDep sources"
+    sudo cp -r rosdep/. /etc/ros/rosdep/
+  else
+    echo "Skipping rosdep sources; no backup"
+  fi
+
+  if [ -d system ];
+  then
+    echo "Restoring Systemd"
+    sudo cp -r system/. /etc/systemd/system
+  else
+    echo "Skipping systemd; no backup"
+  fi
+
+  ############################ HOME FOLDER #############################
+  echo "Restoring Home Folder"
+  cp -r $USERNAME/. ~
+  changeRosDistroFile $OLD_ROSDISTRO $ROSDISTRO ~/.bashrc
+
+  ############################ UDEV #############################
+  if [ -d rules.d ];
+  then
+    echo "Restoring udev rules"
+    sudo cp -r rules.d/. /etc/udev/rules.d
+  else
+    echo "Skipping udev rules; no backup"
+  fi
+
+  ############################ NETWORK #############################
+  echo "Restoring Network Setup"
+  if [ -f interfaces ];
+  then
+    echo "Restoring interfaces"
+    sudo cp interfaces /etc/network/interfaces
+  else
+    echo "Skipping /etc/network/interfaces; no backup"
+  fi
+  if [ -d netplan ];
+  then
+    echo "Restoring netplan files"
+    sudo cp -r netplan/. /etc/netplan
+  else
+    echo "Skipping netplan; no backup"
+  fi
+  if [ -f hostname ];
+  then
+    echo "Restoring hostname"
+    sudo cp hostname /etc/hostname
+  else
+    echo "Skipping /etc/hostname; no backup"
+  fi
+  if [ -f hosts ];
+  then
+    echo "Restoring hosts"
+    sudo cp hosts /etc/hosts
+  else
+    echo "Skipping /etc/hosts; no backup"
+  fi
+  if [ -d iptables ];
+  then
+    echo "Restoring iptables"
+    sudo cp -r iptables/. /etc/iptables
+  else
+    echo "Skipping /etc/iptables; no backup"
+  fi
+
+  ############################ RC.LOCAL #############################
+  if [ -f rc.local ];
+  then
+    echo "Restoring rclocal"
+    sudo cp rc.local /etc/rc.local
+  else
+    echo "Skipping rc.local; no backup"
+  fi
+
+  ############################ USER GROUPS #############################
+  echo "Restoring user groups"
+  while read LINE; do
+    for GROUP in ${LINE}; do
+      echo "Adding user to group $GROUP"
+      echo "sudo usermod -a -G $GROUP $(whoami)"
+      sudo usermod -a -G $GROUP $(whoami)
+    done
+  done < groups
+
+  ############################ APT #############################
+  if [ -d sources.list.d ];
+  then
+    echo "Restoring APT sources"
+    sudo cp -r sources.list.d/. /etc/apt/sources.list.d
+  else
+    echo "Skipping additional APT sources; no backup present"
+  fi
+
+  INSTALL_APT=$(promptDefaultYes "Reinstall APT packages?")
+  if [ $INSTALL_APT == 1 ];
+  then
+    echo "Reinstalling APT packages"
+  else
+    echo "Creating a script to let you reinstall APT packages later"
+    echo "#!/bin/bash" > $HOME/restore-apt.sh
+    echo "# Automatically generated by $0 v$VERSION" >> $HOME/restore-apt.sh
+    echo "# $(date)" >> $HOME/restore-apt.sh
+    echo "sudo apt-get install --reinstall --yes \\" >> $HOME/restore-apt.sh
+    chmod +x $HOME/restore-apt.sh
+  fi
+  while read PKG; do
+    # only reinstall ROS packages; there are too many other packages whose versions/names/etc... may have changed
+    # to be able to reliably reinstall them all
+    if [[ $PKG = ros-$OLD_ROSDISTRO-* ]];
+    then
+      NEW_PKG=$(changeRosDistroPackage "$OLD_ROSDISTRO" "$ROSDISTRO" $PKG)
+
+      if [ $INSTALL_APT == 1 ];
+      then
+        tryInstallApt $NEW_PKG
+      else
+        echo "    $NEW_PKG \\" >> $HOME/restore-apt.sh
+      fi
+    else
+      # other packages we'll try to install, but they may not be available
+      if [ $INSTALL_APT == 1 ];
+      then
+        tryInstallApt $PKG
+      else
+        echo "    $PKG \\" >> $HOME/restore-apt.sh
+      fi
+    fi
+  done < installed_pkgs.list
+
+  ############################ PIP #############################
+  # we have to do pip last, as some pip packages may be provided via apt
+  # this should _only_ install pip packages that were manually installed
+  INSTALL_PIP=$(promptDefaultYes "Restore PIP packages?")
+  if [ $INSTALL_PIP == 1 ];
+  then
+    echo "Restoring pip packages"
+    echo "If you have not already restored APT packages you may encounter errors"
+  else
+    echo "Creating script so you can restore PIP packages later..."
+    echo "#!/bin/bash" > $HOME/restore-pip.sh
+    echo "# Automatically generated by $0 v$VERSION" >> $HOME/restore-pip.sh
+    echo "# $(date)" >> $HOME/restore-pip.sh
+    chmod +x $HOME/restore-pip.sh
+  fi
+  # the default format of pip list is changing, so to keep this script working explicitly use the legacy format if it's not already
+  echo "Setting PIP to use legacy list format"
+  echo "To revert this change, edit $HOME/.config/pip/pip.conf"
+  if [ ! -f $HOME/.config/pip/pip.conf ];
+  then
+    mkdir -p $HOME/.config/pip/
+    touch $HOME/.config/pip/pip.conf
+  fi
+  echo "[list]" >> $HOME/.config/pip/pip.conf
+  echo "format=legacy" >> $HOME/.config/pip/pip.conf
+  while read line; do
+    TOKENS=($line)
+    PKG=${TOKENS[0]}
+    VERSION=$(echo ${TOKENS[1]} | sed 's/[)(]//g')
+
+    PIP_OUT=$(pip list|grep "^$PKG\s")
+    TOKENS=($PIP_OUT)
+    INSTALLED_PKG=${TOKENS[0]}
+    INSTALLED_VERSION=$(echo ${TOKENS[1]} | sed 's/[)(]//g')
+
+    if [ $INSTALL_PIP == 1 ];
+    then
+      # check if the package is installed, if it's not then install it
+      if [[ "$INSTALLED_PKG" == "$PKG" && ("$INSTALLED_VERSION" == "$VERSION" || "$INSTALLED_VERSION" > "$VERSION") ]];
+      then
+        echo "pip package $PKG $VERSION (or newer) is already installed"
+      else
+        echo "Installing pip package $PKG"
+        tryInstallPip $PKG $VERSION
+      fi
+    else
+      # add this package to the install script
+      if [[ "$INSTALLED_PKG" == "$PKG" && ("$INSTALLED_VERSION" == "$VERSION" || "$INSTALLED_VERSION" > "$VERSION") ]];
+      then
+        echo "pip package $PKG $VERSION (or newer) is already installed"
+      else
+        echo "pip install -Iv $PKG==$VERSION" >> $HOME/restore-pip.sh
+      fi
+    fi
+  done < pip.list
+
+  ############################ CLEANUP #############################
+  echo "Done Restoring Files"
+  cd ..
+  cleanup "$CUSTOMER"
+
+  ############################ FINAL MESSAGES #############################
+
+  if [ $INSTALL_APT == 0 ];
+  then
+    echo -e "Run ${GREEN}$HOME/restore-apt.sh${NC} to resintall APT packages"
+  else
+    if [ -f $HOME/could_not_install_apt.sh ];
+    then
+      echo -e "[WARN] ${RED}Some APT packages could not be installed.  See ${GREEN}$HOME/could_not_install_apt.sh${RED} for more details.${NC}"
+    fi
+  fi
+  if [ $INSTALL_PIP == 0 ];
+  then
+    echo -e "Run ${GREEN}$HOME/restore-pip.sh${NC} to resintall PIP packages"
+  else
+    if [ -f $HOME/could_not_install_pip.sh ];
+    then
+      echo -e "[WARN] ${RED}Some PIP packages could not be installed.  See ${GREEN}$HOME/could_not_install_pip.sh${RED} for more details.${NC}"
+    fi
+  fi
+else
+  echo "USAGE: bash restore.sh customer_name [username]"
+fi
